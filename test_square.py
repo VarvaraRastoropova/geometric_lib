0708b1da922884f1cf7a65d87eee16feed46273b
Varvara <varvararastoropova@gmail.com>
2024-11-05 11:38:10 +0300

    Добавлены тесты

---
+++ test_square.py
@@ -0,0 +1,24 @@
+from square import area, perimeter
+
+import unittest
+
+class SquareTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_mul1(self):
+        res = area(3)
+        self.assertEqual(res, 9)
+        
+    def test_mul2(self):
+        res = area(9)
+        self.assertEqual(res, 81)
+
+    def test_add1(self):
+        res = perimeter(3)
+        self.assertEqual(res, 12)
+
+    def test_add2(self):
+        res = perimeter(8)
+        self.assertEqual(res, 32)
