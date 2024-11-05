3601e06ef15bdeed560a93044d5da2ada8feb592
Varvara <varvararastoropova@gmail.com>
2024-11-05 11:37:54 +0300

    Добавлены тесты

---
+++ test_circle.py
@@ -0,0 +1,24 @@
+from circle import area, perimeter
+
+import unittest
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_mul1(self):
+        res = area(3)
+        self.assertEqual(res, 28.274333882308138)
+        
+    def test_mul2(self):
+        res = area(9)
+        self.assertEqual(res, 254.46900494077323)
+
+    def test_add1(self):
+        res = perimeter(3)
+        self.assertEqual(res, 18.84955592153876)
+
+    def test_add2(self):
+        res = perimeter(8)
+        self.assertEqual(res, 50.26548245743669)
