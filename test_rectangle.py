348f1c54de6b3cde8e9f199439b6a8b8fd2cb28a
Varvara <varvararastoropova@gmail.com>
2024-11-05 11:37:13 +0300

    Добавлены тесты

---
+++ test_rectangle.py
@@ -0,0 +1,24 @@
+from rectangle import area, perimeter
+
+import unittest
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+
+    def test_mul1(self):
+        res = area(3, 4)
+        self.assertEqual(res, 12)
+        
+    def test_mul2(self):
+        res = area(9, 5)
+        self.assertEqual(res, 45)
+
+    def test_add1(self):
+        res = perimeter(3, 4)
+        self.assertEqual(res, 14)
+
+    def test_add2(self):
+        res = perimeter(8, 5)
+        self.assertEqual(res, 26)
